2eae3cf07a0086983871648b1c8ac664f3f91db2
DrMaul <agusfmartinez99@gmail.com>
2023-11-07 12:49:41 -0300

    Ultimos errores

--- JS/app.js
+++ JS/app.js
@@ -9,18 +9,23 @@ Mi proyecto consistirá en poder hacer llegar a mayor escala todo el trabajo que
 
 let compraTotal = 0;
 
-
+//Menú principal con el mensaje de bienvenida
 const mensajeBienvenida = () => {
+
     alert("Bienvenido a Ojitos Turcos");
     compraTotal = 0;
     menuCompra();
+
 }
 
+
+//Menú de compra con el listado de productos
 const menuCompra = () => {
 
     let productoName;
     let productoPrecio;
     let opcion;
+
     let invalidProd = false;
     let invalidOption = false;
 
@@ -33,6 +38,9 @@ const menuCompra = () => {
     \n 0- Salir`);
 
 
+    //Selector de productos segun lo ingresado por el usuario
+    //Solo admite como input numeros del 1 al 5 para seleccionar productos, o el 0 para salir al menú principal
+    //Cualquier otro input se considera invalido
     switch (productoID) {
         case "1":
             productoName = "Pulsera de Ojo Turco";
@@ -83,35 +91,48 @@ const menuCompra = () => {
             break;
     }
 
-    
-    switch (opcion) {
-        case "1":
-            agregarAlCarrito(productoName, productoPrecio);
-            break;
-        case "2":
-            menuCompra();
-            break;
-        case "0":
-            alert("Redirigiendo al menú");
-            mensajeBienvenida();
-            break;
-        default:
-            invalidOption = true;
-            break;
-    }   
+    //Al seleccionar un producto te dejará agregarlo al carrito, volver atrás o salir al menú principal.
+    //Cualquier otro input se considera invalido
+    if (productoID != 0 && invalidProd === false) {
+        switch (opcion) {
+            case "1":
+                agregarAlCarrito(productoName, productoPrecio);
+                break;
+            case "2":
+                menuCompra();
+                break;
+            case "0":
+                alert("Redirigiendo al menú");
+                mensajeBienvenida();
+                break;
+            default:
+                invalidOption = true;
+                break;
+            }  
+    }
+     
 
+    //Cuando el input de producto o de opción sean invalidos, se marcan como "true" y te regresan al menú
     if (invalidProd === true || invalidOption === true) {
         alert("Opción inválida. Intente nuevamente");
+        invalidProd = false;
+        invalidOption = false;
         menuCompra();
     }
 
 }
 
+//Función que agrega los productos seleccionados al carrito, recibe el nombre y precio del producto
 const agregarAlCarrito = (productoName, productoPrecio) => {
 
-    
+    //La variable global "compraTotal" acumulará el precio del producto cada vez que se agregue uno al carrito
     compraTotal += productoPrecio;
 
+    //A continuación se podra ingresar un input para finalizar la compra
+    //Seguir comprando acumula productos en el carrito
+    //Volver eliminará el ultimo producto del carrito
+    //Salir regresa al menú principal
+    //Cualquier otro input se considera invalido
     let opcion = prompt(`Estas a punto de comprar ${productoName} por $${productoPrecio}. Total en carrito: $${compraTotal}
             \n A continuación seleccione una opción para continuar:
             \n 1- Finalizar compra
@@ -144,8 +165,11 @@ const agregarAlCarrito = (productoName, productoPrecio) => {
 
 }
 
+//Función para finalizar la compra en carrito
 const comprar = () => {
 
+    //El menú permite ir a la sección de pago, cancelar la compra o salir al menú principal
+    //Cualquier otro input se considera invalido
     let opcion = prompt(`Su compra total es de $${compraTotal}. A continuación seleccione una opción para continuar:
             \n 1- Pagar
             \n 2- Cancelar
@@ -174,6 +198,7 @@ const comprar = () => {
 
 }
 
+//Para realizar el pago se debe completar el formulario
 const formularioDePago = () => {
     let nombre;
     let apellido;
@@ -182,17 +207,24 @@ const formularioDePago = () => {
     let email;
     let intentos = 5;
 
+    //Seteo variables booleanas para controlar que etapa del formulario fue completada correctamente
+    //Inicialmente en "false", luego se activarán como "true" dependiendo si el input fue completado correctamente
     let nomValid = false;
     let apValid= false;
     let telValid = false;
     let dirValid = false;
     let emailValid= false;
 
-
-    alert(`Bienvenido al formulario de pago. Por favor completar de forma correta para evitar inconvenientes.
-    Usted cuenta con 5 intentos para completar el formulario correctamente. Al acabarse los intentos se cancelará su compra.  `)
-
+    
+    //El bucle for itera y descuenta intentos cada vez que se ingresa un input incorrecto hasta llegar a 0 intentos
     for (let i=intentos; i>=0; i--) {
+
+        if (i === intentos) {
+            //Se informa al usuario que cuenta con 5 intentos para completar el formulario. Pasados los 5 intentos se cancela la compra.
+            alert(`Bienvenido al formulario de pago. Por favor completar de forma correta para evitar inconvenientes.
+            Usted cuenta con 5 intentos para completar el formulario correctamente. Al acabarse los intentos se cancelará su compra.  `)
+
+        }
 
         alert(`Intentos restantes: ${i}`);
         if (i === 0) {
@@ -200,6 +232,8 @@ const formularioDePago = () => {
             mensajeBienvenida();
         }
 
+        //Primero llamo a validarInfo, luego valido para cada caso particular si cumple el formato esperado
+        //Utilizo "continue" en cuanto el input sea invalido para que saltee todas las instrucciones y vuelva al comienzo del for, descontando un intento
         if (nomValid === false) {
             nombre = prompt(`Ingrese su nombre`);
             if (validarInfo(nombre)!==true){
@@ -262,11 +296,14 @@ const formularioDePago = () => {
             emailValid =true;
         }
 
+        //En caso de que todos los input hayan sido válidos, con "break" salgo del for 
         break;
         
     }
     
-
+    //Por último, solicito que confirme su compra validando que la información ingresada haya sido correcta
+    //Si fue correcta se procesa la compra, si no fue correcta permite completar el formulario de nuevo, 0 para salir
+    //Cualquier otro input se considera invalido
     let confirmar = prompt(`Su pedido de $${compraTotal}: a nombre de ${apellido} ${nombre}, con telefono ${tel} y email: ${email}, espera recibir su pedido en el domicilio ${direc}.
     \n Esta información es correcta?
     \n 1- SI
@@ -284,9 +321,14 @@ const formularioDePago = () => {
             alert("Redirigiendo al formulario. Por favor completar de forma correta para evitar inconvenientes");
             formularioDePago();
             break;
-        default:
+        case "0":
             alert("Compra cancelada. Redirigiendo al menú");
             mensajeBienvenida();
+            break;
+        default:
+            alert("Opción invalida. Intente nuevamente");
+            alert("Redirigiendo al formulario. Por favor completar de forma correta para evitar inconvenientes");
+            formularioDePago();
             break;
     }  
 }
